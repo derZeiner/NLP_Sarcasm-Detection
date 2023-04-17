b15c10418d9eed511df70582b547830f26508016
Michael Zeiner <56203741+derZeiner@users.noreply.github.com>
2023-04-17 22:00:59 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,8 +3,21 @@ import train_model.train_scratch_sarcasm_model as ssm
 import train_model.train_scratch_kfold as sskm
 
 
-
+# ***********************************
+# Function: Main function
+# ***********************************
+# which model do you want to train?
+train_bpm = True
+train_ssm = True
+train_sskm = True
 
 if __name__ == '__main__':
+    if train_ssm:
+        ssm.run()
+    if train_sskm:
+        sskm.run()
+    if train_bpm:
+        pbm.run()
 
-    pbm.run()
+
+        
